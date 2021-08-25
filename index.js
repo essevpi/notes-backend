bea31a5af204f1d13ae72ca09c70f97e21b67ab1
Simone <essevpi@gmail.com>
2021-08-25 19:32:15 +0200

    notes + mongodb update - part 3

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const Note = require('./models/note');
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
+app.use(express.static('build'));
+app.use(express.json());
+app.use(cors());
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if(error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'});
+  } else if (error.name === 'ValidationError') {
+    return response.status(404).send({error: error.message})
+  }
+
+  next(error);
 }
 
-app.use(express.json());
-app.use(express.static('build'));
-app.use(cors());
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+}
+
 app.use(requestLogger);
 
 let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
-    }
-  ];
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
+];
 
   const generateId = () => {
     const maxId = notes.length > 0
@@ -45,62 +60,88 @@ let notes = [
   }
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>');
+  response.send('<h1>Hello World!</h1>');
 });
 
+//Get all notes
 app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
     response.json(notes);
+  });
 });
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find(n => n.id === id);
-
-    if (note) {
+//Get specific note
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
         response.json(note);
-    } else {
+      } else {
         response.status(404).end();
-    }
+      }
+    })
+    .catch(error => next(error));
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
-
-    response.status(204).end();
+//Delete a note
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+      .then(result=> {
+        response.status(204).end();
+      })
+      .catch(error => next(error));
 });
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body;
-  
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    });
-  }
+//Create a note
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
 
-  const note = {
+  /* if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  } */
+
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId()
-  }
+  })
 
-  notes = notes.concat(note);
-  
-  response.json(note);
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+  .catch(error => next(error));
 });
 
+//Update importance of note
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    .then(updatedNote => {
+      response.json(note);
+    })
+    .catch(error => next(error));
+    
+});
+
+//Page not found
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 }
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`)
+  console.log(`Server is running on port ${PORT}.`);
 });
 
 
